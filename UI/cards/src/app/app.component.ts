082f5b81ca3bdbab66638ac28811772c73f91588
luisjavierluna <javierlunaluis@gmail.com>
2022-08-29 16:11:47 -0500

    Added update card method

--- UI/cards/src/app/app.component.ts
+++ UI/cards/src/app/app.component.ts
@@ -33,6 +33,14 @@ export class AppComponent implements OnInit {
   }
 
   onSubmit(){
+    if (this.card.id == '') {
+      this.addCard();
+    }else{
+      this.updateCard(this.card);
+    }
+  }
+
+  addCard(){
     this.cardsService.addCard(this.card)
     .subscribe(
       response => {
@@ -51,6 +59,19 @@ export class AppComponent implements OnInit {
 
   deleteCard(id: string){
     this.cardsService.deleteCard(id)
+    .subscribe(
+      response =>{
+        this.getAllCards();
+      }
+    )
+  }
+
+  populateCard(card: Card){
+    this.card = card;
+  }
+
+  updateCard(card: Card){
+    this.cardsService.updateCard(card)
     .subscribe(
       response =>{
         this.getAllCards();
