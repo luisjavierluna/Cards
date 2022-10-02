4327ccf23f0e1249e9fd77aac8dbf58a4496a448
luisjavierluna <javierlunaluis@gmail.com>
2022-10-02 14:24:09 -0500

    Added the validations of all the fields

--- UI/cards/src/app/app.component.ts
+++ UI/cards/src/app/app.component.ts
@@ -28,11 +28,41 @@ export class AppComponent implements OnInit {
     this.getAllCards();
     
     this.form = this.formBuilder.group({
-      cardHolderName: ['', {validators: [Validators.required]}],
-      cardNumber: ['', {validators: [Validators.required]}],
-      expiryMonth: ['', {validators: [Validators.required]}],
-      expiryYear: ['', {validators: [Validators.required]}],
-      cvc: ['', {validators: [Validators.required]}],
+      cardHolderName: ['', 
+        [
+          Validators.required
+        ]
+      ],
+      cardNumber: ['', 
+        [
+          Validators.required, 
+          Validators.minLength(16),
+          Validators.maxLength(16),
+        ]
+      ],
+      expiryMonth: ['', 
+        [
+          Validators.required,
+          Validators.minLength(1),
+          Validators.maxLength(2),
+          Validators.min(1),
+          Validators.max(12)
+        ]
+      ],
+      expiryYear: ['', 
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(2),
+        ]
+      ],
+      cvc: ['', 
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(3),
+        ]
+      ],
     })
   }
 
@@ -46,8 +76,10 @@ export class AppComponent implements OnInit {
   onSubmit(){
     if (this.card.id == '') {
       this.addCard();
+      // HERE IS NECESARY TO RELOAD THE PAGE
     }else{
       this.updateCard(this.card);
+      // HERE IS NECESARY TO RELOAD THE PAGE
     }
   }
 
@@ -105,5 +137,79 @@ export class AppComponent implements OnInit {
         }
       }
     )
+  }
+
+  // Comentar cuendo se requieran probar las mismas validaciones en el backend
+  getFieldNameError(){
+    var cardHolderName = this.form.get('cardHolderName')
+
+    if(cardHolderName?.hasError('required') && cardHolderName?.touched){
+      return '- The field Card Holder Name is required'
+    } 
+
+    return '';
+  }
+
+  getFieldNumberError(){
+    var cardNumber = this.form.get('cardNumber')
+
+    if(cardNumber?.touched){
+      if (cardNumber?.hasError('required')){
+        return '- The field Card Number is required'
+      } 
+      else if (cardNumber?.hasError('minlength') || cardNumber?.hasError('maxlength')) {
+        return '- The field Card Number must have 16 characteres'
+      }
+    }
+
+    return ''
+  }
+
+  getFieldMonthError(){
+    var expiryMonth = this.form.get('expiryMonth')
+
+    if(expiryMonth?.touched){
+      if (expiryMonth?.hasError('required')){
+        return '- The field Expiry Month is required'
+      } 
+      else if (expiryMonth?.hasError('min') || expiryMonth?.hasError('max')) {
+        return '- The value of the field Expiry Month must be between 1 and 12'
+      }      
+      else if (expiryMonth?.hasError('minlength') || expiryMonth?.hasError('maxlength')) {
+        return '- The field Expiry Month must be between 1 and 2 characteres'
+      }
+    }
+
+    return ''
+  }
+
+  getFieldYearError(){
+    var expiryYear = this.form.get('expiryYear')
+
+    if(expiryYear?.touched){
+      if (expiryYear?.hasError('required')){
+        return '- The field Expiry Year is required'
+      } 
+      else if (expiryYear?.hasError('minlength') || expiryYear?.hasError('maxlength')) {
+        return '- The field Expiry Year must have 2 characteres'
+      }
+    }
+
+    return ''
+  }
+
+  getFieldCVCError(){
+    var cvc = this.form.get('cvc')
+
+    if(cvc?.touched){
+      if (cvc?.hasError('required')){
+        return '- The field CVC is required'
+      } 
+      else if (cvc?.hasError('minlength') || cvc?.hasError('maxlength')) {
+        return '- The field CVC must have 3 characteres'
+      }
+    }
+
+    return ''
   }
 }
